4503f703955331a8e2283805bd94d74b456e707a
Márcio Duarte <maethorin@gmail.com>
2014-10-17 19:44:21 -0300

    Removendo view!

--- eleicoes/urls.py
+++ eleicoes/urls.py
@@ -12,6 +12,5 @@ urlpatterns = patterns(
     url(r'^escolher/(?P<estado>[A-Z]{2})$', 'santinho.views.escolher_candidatos', name='escolher_candidatos'),
     url(r'^santinho/(?P<estado>[A-Z]{2})/(?P<presidente>(\d{2}|XX))/(?P<governador>(\d{2}|XX))/(?P<senador>(\d{3}|XXX))/(?P<deputado_federal>(\d{4}|XXXX))/(?P<deputado_estadual>(\d{5}|XXXXX))$', 'santinho.views.criar', name='criar_santinho'),
     url(r'^resultado/(?P<estado>[A-Z]{2})/(?P<presidente>(\d{2}|XX))/(?P<governador>(\d{2}|XX))/(?P<senador>(\d{3}|XXX))/(?P<deputado_federal>(\d{4}|XXXX))/(?P<deputado_estadual>(\d{5}|XXXXX))$', 'santinho.views.resultado', name='resultado'),
-    url(r'^adicionar_fotos$', 'santinho.views.adiciona_fotos', name='adiciona_fotos'),
     url(r'^admin/', include(admin.site.urls)),
 )
